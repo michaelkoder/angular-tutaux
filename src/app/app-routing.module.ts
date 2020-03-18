d1eb38a2cd1f854918b9f44b611c33ecf98d2d89
michaelkoder <michael.claude.web@gmail.com>
2020-03-18 16:36:20 +0100

    init tutaux project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent} from './home/home.component'
+import { HttpPageComponent} from './http-page/http-page.component'
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'httpPage',component:HttpPageComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'**',component:HomeComponent}
+]; 
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { HttpPageComponent } from './http-page/http-page.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    HttpPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
